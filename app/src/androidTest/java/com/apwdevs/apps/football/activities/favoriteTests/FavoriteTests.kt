9d9699d7cbb5a4769a1ded77c0c5c05213c7c039
Alexzander Purwoko Widiantoro <purwoko908@gmail.com>
2019-01-16 14:00:42 +0700

    update partial

--- app/src/androidTest/java/com/apwdevs/apps/football/activities/favoriteTests/FavoriteTests.kt
+++ app/src/androidTest/java/com/apwdevs/apps/football/activities/favoriteTests/FavoriteTests.kt
@@ -10,19 +10,53 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class FavoriteTests {
-
+    
+    // THIS CLASS IS JUST PERFORM Favorite Testing, no another action 
+    
     @Rule
     @JvmField
     var mActivityRule: ActivityTestRule<SplashScreenActivity> =
         ActivityTestRule(SplashScreenActivity::class.java, false, false)
-
+    
+    private val dataResponse: MutableList<TeamLeagueData> = mutableListOf()
     @Before
     fun setUp() {
-
+        val response = SplashPresenterImpl.getData()
+        dataResponse.addAll(response.leagues)
     }
 
     @Test
     fun testFavorite() {
-
+        val i = Intent()
+        i.putExtra(ParameterClass.KEY_IS_APP_TESTING, true)
+        i.putExtra(ParameterClass.LIST_LEAGUE_DATA, LeagueResponse(dataResponse))
+        mActivityRule.launchActivity(i)
+        Thread.sleep(ThreadTimes.MIDDLE_TIME)
+        
+        checkFavoriteMenuMustBeNull()
+        performAddFavoriteInLastMatch()
+        performAddFavoriteInNextMatch()
+        performAddFavoriteInTeamsMenu()
+        performClearFavoriteMenuMatchTab()
+        performClearFavoriteMenuTeamsTab()
+        checkFavoriteMenuMustBeNull()
+    }
+    fun checkFavoriteMenuMustBeNull(){
+        
+    }
+    fun performAddFavoriteInLastMatch(){
+        
+    }
+    fun performAddFavoriteInNextMatch(){
+        
+    }
+    fun performAddFavoriteInTeamsMenu(){
+        
+    }
+    fun performClearFavoriteMenuMatchTab(){
+        
+    }
+    fun performClearFavoriteMenuTeamsTab(){
+        
     }
 }
